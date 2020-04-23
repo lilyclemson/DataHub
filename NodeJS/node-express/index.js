63393c375bb314dacea1032b91f74a8ec79dc912
lilyclemson <lilix@g.clemson.edu>
2020-04-23 00:46:45 -0400

    simple REST

--- NodeJS/node-express/index.js
+++ NodeJS/node-express/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -8,6 +9,48 @@ const port = 3000;
 const app = express();
 
 app.use(morgan('dev'));
+app.use(bodyParser.json())
+
+app.all('/dishes', (req, res, next)=>{
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  next();
+});
+
+app.get('/dishes', (req, res, next) =>{
+  res.end('will send all the dishes to you!');
+});
+
+app.post('/dishes', (req, res, next)=>{
+  res.end('Will add the dish:' +  req.body.name + ' with details: ' + req.body.description);
+});
+
+app.put('/dishes', (req, res, next)=>{
+  res.sendStatuscode = 403;
+  res.end('PUT operation not supported on /dishes');
+});
+
+app.delete('/dishes', (req, res, next)=>{
+  res.end('Deleting all the dishes!');
+});
+
+
+app.get('/dishes/:dishid', (req, res, next) =>{
+  res.end('will send the details of the dish' + req.params.dishid+ ' to you!');
+});
+
+app.post('/dishes/:dishid', (req, res, next)=>{
+  res.sendStatuscode = 403;
+  res.end('POST operation not supported on /dishes' + req.params.dishId);});
+
+app.put('/dishes/:dishid', (req, res, next)=>{
+  res.write('Update the dish ' + req.params.dishid);
+  res.end('Will update the dish: ' + req.body.name + ' with details '+ req.body.description);
+});
+
+app.delete('/dishes/:dishid', (req, res, next)=>{
+  res.end('Deleting the dish' + req.params.dishid + '!');
+});
 
 app.use(express.static(__dirname + '/public'));
 
